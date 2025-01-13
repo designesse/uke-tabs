d87f70455c09da76318c72d1dda101ed8627323c
Malina Randrianavony <1680885+designesse@users.noreply.github.com>
2025-01-13 10:18:51 -0700

    Add Nothing Else Matters' chords

--- src/songs/NothingElseMatters.tsx
+++ src/songs/NothingElseMatters.tsx
@@ -1,94 +1,106 @@
+import Chord from "../components/chord.tsx";
+
 const NothingElseMatters = () =>
     <>
         <h1> Nothing Else Matters - Metallica </h1>
 
+        <h2>Instrumental</h2>
+        <div className="marg-bot">
+            <div className="pos-rel-top">---</div>
+        </div>
+
         <h2>Verse 1</h2>
         <div className="marg-bot">
-            <div className="pos-rel-top">So close no matter how far</div>
-            <div className="pos-rel-top">Could not be much far from the heart</div>
-            <div className="pos-rel-top">Forever trusting who we are</div>
-            <div className="pos-rel-top">And nothing else matters</div>
+            <div className="pos-rel-top"><Chord name="Em"/> So close no matter <Chord name="D"/> how far <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="Em"/> Could not be much far <Chord name="D"/> from the heart <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="Em"/> Forever trusting <Chord name="D"/> who we are <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="G"/> And <Chord name="B7"/> nothing else matters <Chord name="Em"/></div>
         </div>
 
         <h2>Verse 2</h2>
         <div className="marg-bot">
-            <div className="pos-rel-top">Never opened myself this way</div>
-            <div className="pos-rel-top">Life is ours, we live it our way</div>
-            <div className="pos-rel-top">All these words I don't just say</div>
-            <div className="pos-rel-top">And nothing else matters</div>
+            <div className="pos-rel-top"><Chord name="Em"/> Never opened my<Chord name="D"/>self this way <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="Em"/> Life is ours, we live it <Chord name="D"/> our way <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="Em"/> All these words I don't <Chord name="D"/> just say <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="G"/> And <Chord name="B7"/> nothing else matters <Chord name="Em"/></div>
         </div>
 
         <h2>Verse 3</h2>
         <div className="marg-bot">
-            <div className="pos-rel-top">Trust I seek and I find in you</div>
-            <div className="pos-rel-top">Everyday for us something new</div>
-            <div className="pos-rel-top">Open mind for a different view</div>
-            <div className="pos-rel-top">And nothing else matters</div>
+            <div className="pos-rel-top"><Chord name="Em"/> Trust I seek and I <Chord name="D"/> find in you <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="Em"/> Everyday for us <Chord name="D"/> something new <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="Em"/> Open mind for a <Chord name="D"/> different view <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="G"/> And <Chord name="B7"/> nothing else matters <Chord name="Em"/></div>
         </div>
 
         <h2>Chorus</h2>
         <div className="marg-bot">
-            <div className="pos-rel-top">Never cared for what they do</div>
-            <div className="pos-rel-top">Never cared for what they know</div>
-            <div className="pos-rel-top">But I know</div>
+            <div className="pos-rel-top"><Chord name="D"/>Never cared for what they <Chord name="C"/> do <Chord name="A"/></div>
+            <div className="pos-rel-top"><Chord name="D"/>Never cared for what they <Chord name="C"/> know <Chord name="A"/></div>
+            <div className="pos-rel-top"><Chord name="D"/>But I <Chord name="Em"/>know</div>
         </div>
 
         <h2>Verse 4</h2>
         <div className="marg-bot">
-            <div className="pos-rel-top">So close no matter how far</div>
-            <div className="pos-rel-top">Could not be much far from the heart</div>
-            <div className="pos-rel-top">Forever trusting who we are</div>
-            <div className="pos-rel-top">And nothing else matters</div>
+            <div className="pos-rel-top"><Chord name="Em"/> So close no matter <Chord name="D"/> how far <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="Em"/> Could not be much far <Chord name="D"/> from the heart <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="Em"/> Forever trusting <Chord name="D"/> who we are <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="G"/> And <Chord name="B7"/> nothing else matters <Chord name="Em"/></div>
         </div>
 
         <h2>Chorus</h2>
         <div className="marg-bot">
-            <div className="pos-rel-top">Never cared for what they do</div>
-            <div className="pos-rel-top">Never cared for what they know</div>
-            <div className="pos-rel-top">But I know</div>
+            <div className="pos-rel-top"><Chord name="D"/>Never cared for what they <Chord name="C"/> do <Chord name="A"/></div>
+            <div className="pos-rel-top"><Chord name="D"/>Never cared for what they <Chord name="C"/> know <Chord name="A"/></div>
+            <div className="pos-rel-top"><Chord name="D"/>But I <Chord name="Em"/>know</div>
         </div>
 
         <h2>Instrumental</h2>
         <div className="marg-bot">
-            <div className="pos-rel-top"></div>
+            <div className="pos-rel-top">---</div>
         </div>
 
         <h2>Verse 5</h2>
         <div className="marg-bot">
-            <div className="pos-rel-top">Never opened myself this way</div>
-            <div className="pos-rel-top">Life is ours, we live it our way</div>
-            <div className="pos-rel-top">All these words I don't just say</div>
-            <div className="pos-rel-top">And nothing else matters</div>
+            <div className="pos-rel-top"><Chord name="Em"/> Never opened my<Chord name="D"/>self this way <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="Em"/> Life is ours, we live it <Chord name="D"/> our way <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="Em"/> All these words I don't <Chord name="D"/> just say <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="G"/> And <Chord name="B7"/> nothing else matters <Chord name="Em"/></div>
         </div>
 
         <h2>Verse 6</h2>
         <div className="marg-bot">
-            <div className="pos-rel-top">Trust I seek and I find in you</div>
-            <div className="pos-rel-top">Everyday for us something new</div>
-            <div className="pos-rel-top">Open mind for a different view</div>
-            <div className="pos-rel-top">And nothing else matters</div>
+            <div className="pos-rel-top"><Chord name="Em"/> Trust I seek and I <Chord name="D"/> find in you <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="Em"/> Everyday for us <Chord name="D"/> something new <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="Em"/> Open mind for a <Chord name="D"/> different view <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="G"/> And <Chord name="B7"/> nothing else matters <Chord name="Em"/></div>
         </div>
 
         <h2>Chorus</h2>
         <div className="marg-bot">
-            <div className="pos-rel-top">Never cared for what they say</div>
-            <div className="pos-rel-top">Never cared for games they play</div>
-            <div className="pos-rel-top">Never cared for games they do</div>
-            <div className="pos-rel-top">Never cared for what they know</div>
-            <div className="pos-rel-top">And I know</div>
+            <div className="pos-rel-top"><Chord name="D"/>Never cared for what they <Chord name="C"/> say <Chord name="A"/></div>
+            <div className="pos-rel-top"><Chord name="D"/>Never cared for games they <Chord name="C"/> play <Chord name="A"/></div>
+            <div className="pos-rel-top"><Chord name="D"/>Never cared for games they <Chord name="C"/> do <Chord name="A"/></div>
+            <div className="pos-rel-top"><Chord name="D"/>Never cared for what they <Chord name="C"/> know <Chord name="A"/></div>
+            <div className="pos-rel-top"><Chord name="D"/>And I <Chord name="Em"/>know</div>
         </div>
 
         <h2>Instrumental</h2>
         <div className="marg-bot">
-            <div className="pos-rel-top"></div>
+            <div className="pos-rel-top">---</div>
         </div>
 
-        <h2>Verse 4</h2>
+        <h2>Verse 7</h2>
         <div className="marg-bot">
-            <div className="pos-rel-top">So close no matter how far</div>
-            <div className="pos-rel-top">Could not be much far from the heart</div>
-            <div className="pos-rel-top">Forever trusting who we are</div>
-            <div className="pos-rel-top">No nothing else matters</div>
+            <div className="pos-rel-top"><Chord name="Em"/> So close no matter <Chord name="D"/> how far <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="Em"/> Could not be much far <Chord name="D"/> from the heart <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="Em"/> Forever trusting <Chord name="D"/> who we are <Chord name="C"/></div>
+            <div className="pos-rel-top"><Chord name="G"/> And <Chord name="B7"/> nothing else matters <Chord name="Em"/></div>
+        </div>
+
+        <h2>Instrumental</h2>
+        <div className="marg-bot">
+            <div className="pos-rel-top">---</div>
         </div>
     </>
 
